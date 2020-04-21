2457554750dfa7e567d5c36cc69ec0c9b99aa0b0
Arne Schwabe <arne@rfc2549.org>
2020-04-21 16:45:59 +0200

    Remove import message in mk files

--- crypto/buildinf.h
+++ crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-x86_64"
-#define DATE "built on: Wed Apr  1 15:46:26 2020 UTC"
+#define DATE "built on: Tue Apr 21 14:45:17 2020 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
